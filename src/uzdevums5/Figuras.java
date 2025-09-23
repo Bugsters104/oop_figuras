34c343f86e8fe21eb2cc552a9d0f276914eea38a
Bugsters104 <vards.uzvards@pasts.lv>
2025-09-23 12:18:56 +0300

    Viss sagatavots, lai sāktu izstrādāt izvēlni

---
+++ src/uzdevums5/Figuras.java
@@ -0,0 +1,29 @@
+package uzdevums5;
+
+public class Figuras {
+
+	public static void main(String[] args) {
+		// izveido Centrs objektu
+		Centrs centraP1 = new Centrs(-10, 12);
+		System.out.println(centraP1.izvaditKoordinatas());
+		centraP1.parvietot(5, -2);
+		System.out.println(centraP1.izvaditKoordinatas());
+		
+		// izveido Cetrsturis objektu
+		Cetrsturis cetrsturis1 = new Cetrsturis();
+		System.out.println(cetrsturis1.izvaditCetrsturaInfo());
+		// izvadīt šī četrstūra atrašanās vietu
+		System.out.println(cetrsturis1.punkts.izvaditKoordinatas());
+		
+		Cetrsturis cetrsturis2 = new Cetrsturis(centraP1, 5, 11);
+		System.out.println(cetrsturis2.izvaditCetrsturaInfo()+"\n"+
+				cetrsturis2.punkts.izvaditKoordinatas()+
+				"\ns = "+cetrsturis2.cLaukums());
+		
+		Centrs punkts2 = new Centrs (5, 23);
+		Aplis aplis1 = new Aplis(punkts2, 5);
+		System.out.println(aplis1.izvaditAplaInfo()+"\n"+
+		"s = "+aplis1.rLaukums()+"\n"+
+			"c = "+ aplis1.rLGarums());
+	}
+}
